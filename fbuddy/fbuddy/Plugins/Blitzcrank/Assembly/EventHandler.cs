2a0fe7d95b6a8c4c3679ca867fa882095bfc3a29
Trump <hareketlimisin@gmail.com>
2017-02-13 17:56:38 +0300

    SDK source code

--- fbuddy/fbuddy/Plugins/Blitzcrank/Assembly/EventHandler.cs
+++ fbuddy/fbuddy/Plugins/Blitzcrank/Assembly/EventHandler.cs
@@ -5,13 +5,27 @@ using System.Linq;
 using System.Text;
 using System.Drawing;
 using System.Threading.Tasks;
+using System;
 
 namespace fbuddy.Plugins.Blitzcrank.Assembly
 {
     public static class EventHandler
     {
         public static AIHeroClient Blitzcrank => Player.Instance;
+        private static void Load()
+        {
+            Drawing.OnDraw += Drawing_OnDraw;
+            Game.OnTick += Game_OnTick;
+        }
+        private static void Game_OnTick(EventArgs args)
+        {
 
+        }
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+
+        }
+        
         
     }
 }
